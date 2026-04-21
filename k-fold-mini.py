54ecc72f9e02d2179e371dfd4106292f7b3af0d7
Akhilesh99977 <akhileshrathore315@gmail.com>
2026-04-21 13:22:24 +0530

    Update k-fold-mini.py

--- k-fold-mini.py
+++ k-fold-mini.py
@@ -1,1 +1,21 @@
 
+from sklearn.cluster import KMeans
+import numpy as np
+
+# Sample data
+X = np.array([[1, 2],
+              [1, 4],
+              [1, 0],
+              [10, 2],
+              [10, 4],
+              [10, 0]])
+
+# Model
+kmeans = KMeans(n_clusters=2)
+
+# Train
+kmeans.fit(X)
+
+# Output
+print("Labels:", kmeans.labels_)
+print("Centroids:", kmeans.cluster_centers_)
